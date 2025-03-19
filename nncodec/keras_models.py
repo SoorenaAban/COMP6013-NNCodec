26cfb3b50b46e0712836e7dbd327f17afe181d03
Soorena <60492160+SoorenaAban@users.noreply.github.com>
2025-03-19 01:01:39 +0000

    added a GRU model using same structure as the LSTM model

--- nncodec/keras_models.py
+++ nncodec/keras_models.py
@@ -115,6 +115,93 @@ class LstmKerasModel(TFKerasModelBase):
         logits = self.dense(layer_input_final)
         return self.softmax(logits)
 
+import tensorflow as tf
+
+class GruKerasModel(TFKerasModelBase):
+    """GRU-based Keras subclass model mimicking the LSTM model architecture."""
+    def __init__(self, vocab_size, **kwargs):
+        super(GruKerasModel, self).__init__(**kwargs)
+        self.batch_size = 256
+        self.seq_length = 15
+        self.num_layers = 6
+        self.rnn_units = 1000
+        self.embedding_size = 1024
+        self.vocab_size = vocab_size
+
+        self.embedding = tf.keras.layers.Embedding(
+            input_dim=self.vocab_size,
+            output_dim=self.embedding_size,
+            embeddings_initializer=tf.keras.initializers.RandomUniform(seed=self.seed),
+            name="embedding"
+        )
+
+        self.gru_layers = []
+        for i in range(self.num_layers):
+            gru_layer = tf.keras.layers.GRU(
+                units=self.rnn_units,
+                return_sequences=True,
+                return_state=True,
+                # activation='tanh',
+                # recurrent_activation='sigmoid',
+                kernel_initializer=tf.keras.initializers.GlorotUniform(seed=self.seed + i),
+                recurrent_initializer=tf.keras.initializers.GlorotUniform(seed=self.seed + self.num_layers + i),
+                name=f"gru_{i}"
+            )
+            self.gru_layers.append(gru_layer)
+
+        self.last_time_steps = [
+            GruKerasModel.LastTimeStep(name=f"final_output_{i}")
+            for i in range(self.num_layers)
+        ]
+
+        self.dense = tf.keras.layers.Dense(
+            self.vocab_size,
+            kernel_initializer=tf.keras.initializers.GlorotUniform(seed=self.seed + 2 * self.num_layers),
+            name='dense_logits'
+        )
+        self.softmax = tf.keras.layers.Activation('softmax', dtype='float32', name='predictions')
+
+    class LastTimeStep(tf.keras.layers.Layer):
+        def call(self, inputs):
+            return inputs[:, -1, :]
+
+    @tf.function
+    def call(self, inputs, training=False):
+        """
+        Expected inputs: a list of 1 + 2 * num_layers tensors.
+          - inputs[0]: the main input (token sequences)
+          - For each GRU layer, two dummy states are provided (for compatibility),
+            but only the first is used.
+        """
+        expected_inputs = 1 + 2 * self.num_layers
+        if not isinstance(inputs, (list, tuple)) or len(inputs) != expected_inputs:
+            raise ValueError(f"Expected {expected_inputs} inputs, got {len(inputs)}")
+
+        main_input = inputs[0]
+        embedded = self.embedding(main_input)
+        skip_connections = []
+
+        init_state_0 = inputs[1]  
+        x, _ = self.gru_layers[0](embedded, initial_state=[init_state_0], training=training)
+        skip_connections.append(x)
+
+        for i in range(1, self.num_layers):
+            concatenated = tf.keras.layers.concatenate(
+                [embedded, skip_connections[i-1]],
+                name=f"concat_{i}"
+            )
+            init_state = inputs[1 + 2 * i]  
+            x, _ = self.gru_layers[i](concatenated, initial_state=[init_state], training=training)
+            skip_connections.append(x)
+
+        final_outputs = [self.last_time_steps[i](skip_connections[i]) for i in range(self.num_layers)]
+        if self.num_layers == 1:
+            layer_input_final = final_outputs[0]
+        else:
+            layer_input_final = tf.keras.layers.concatenate(final_outputs, name="final_concat")
+        logits = self.dense(layer_input_final)
+        return self.softmax(logits)
+
 
 class TFPredictionTestingKerasModel(TFKerasModelBase):
     def __init__(self, vocab_size, **kwargs):
